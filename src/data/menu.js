995ee1f8d42674251a57f6dd8e4c2f656e5cf19f
Agnyh Helena <agnyhhelena9@gmail.com>
2026-09-10 17:02:19 -0300

    agua com gas

--- src/data/menu.js
+++ src/data/menu.js
@@ -112,7 +112,8 @@ export const MENU = [
       { id: "beb-aguag", name: "Água com Gás", price: 3.5 },
       { id: "beb-coca", name: "Coca-Cola", price: 6.5 },
       { id: "beb-guarana", name: "Guaraná", price: 5 },
-      { id: "beb-romarinho", name: "Romarinho", price: 4.5 },
+      { id: "beb-romarinho", name: "Romarinho Antartica", price: 4.5 },
+      { id: "beb-romarinho", name: "Romarinho Brahma", price: 4.5 },
     ],
   },
 ];
